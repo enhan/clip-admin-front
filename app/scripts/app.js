30010524cc46d73ac2c07ffcb80439285b75f422
Emmanuel Nhan <nhanmanu@gmail.com>
2014-11-27 23:04:14 +0100

    basic angular app, made up by Yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name clipAdminFrontApp
+ * @description
+ * # clipAdminFrontApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('clipAdminFrontApp', [
+    'ngCookies',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clipAdminFrontApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the clipAdminFrontApp
+ */
+angular.module('clipAdminFrontApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
